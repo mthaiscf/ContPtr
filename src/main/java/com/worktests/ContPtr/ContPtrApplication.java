7e3675e1d198152d7d0610f304823bb42c254ec5
unknown <marques.thaiscf@gmail.com>
2020-08-15 09:44:20 -0300

    CriacaoProjeto

---
+++ src/main/java/com/worktests/ContPtr/ContPtrApplication.java
@@ -0,0 +1,13 @@
+package com.worktests.ContPtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContPtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContPtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worktests/ContPtr/ContPtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worktests.ContPtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContPtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
